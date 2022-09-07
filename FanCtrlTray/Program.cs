81f75df8b5e46ec55f78ad4e94b66da2d8f03278
Faheem Pervez <trippin1@gmail.com>
2022-09-07 10:35:04 +0100

    Live-update fan speed while menu is open

--- FanCtrlTray/Program.cs
+++ FanCtrlTray/Program.cs
@@ -7,6 +7,106 @@ using System.Windows.Forms;
 
 namespace FanCtrlTray
 {
+#if DEBUG
+    // Pavlo K: https://stackoverflow.com/a/48864902
+    using Microsoft.Win32.SafeHandles;
+    using System.IO;
+
+    static class WinConsole
+    {
+        static public void Initialize(bool alwaysCreateNewConsole = true)
+        {
+            bool consoleAttached = true;
+            if (alwaysCreateNewConsole
+                || (AttachConsole(ATTACH_PARRENT) == 0
+                && Marshal.GetLastWin32Error() != ERROR_ACCESS_DENIED))
+            {
+                consoleAttached = AllocConsole() != 0;
+            }
+
+            if (consoleAttached)
+            {
+                InitializeOutStream();
+                InitializeInStream();
+            }
+        }
+
+        private static void InitializeOutStream()
+        {
+            var fs = CreateFileStream("CONOUT$", GENERIC_WRITE, FILE_SHARE_WRITE, FileAccess.Write);
+            if (fs != null)
+            {
+                var writer = new StreamWriter(fs) { AutoFlush = true };
+                Console.SetOut(writer);
+                Console.SetError(writer);
+            }
+        }
+
+        private static void InitializeInStream()
+        {
+            var fs = CreateFileStream("CONIN$", GENERIC_READ, FILE_SHARE_READ, FileAccess.Read);
+            if (fs != null)
+            {
+                Console.SetIn(new StreamReader(fs));
+            }
+        }
+
+        private static FileStream CreateFileStream(string name, uint win32DesiredAccess, uint win32ShareMode,
+                                FileAccess dotNetFileAccess)
+        {
+            var file = new SafeFileHandle(CreateFileW(name, win32DesiredAccess, win32ShareMode, IntPtr.Zero, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, IntPtr.Zero), true);
+            if (!file.IsInvalid)
+            {
+                var fs = new FileStream(file, dotNetFileAccess);
+                return fs;
+            }
+            return null;
+        }
+
+    #region Win API Functions and Constants
+        [DllImport("kernel32.dll",
+            EntryPoint = "AllocConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern int AllocConsole();
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "AttachConsole",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern UInt32 AttachConsole(UInt32 dwProcessId);
+
+        [DllImport("kernel32.dll",
+            EntryPoint = "CreateFileW",
+            SetLastError = true,
+            CharSet = CharSet.Auto,
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr CreateFileW(
+              string lpFileName,
+              UInt32 dwDesiredAccess,
+              UInt32 dwShareMode,
+              IntPtr lpSecurityAttributes,
+              UInt32 dwCreationDisposition,
+              UInt32 dwFlagsAndAttributes,
+              IntPtr hTemplateFile
+            );
+
+        private const UInt32 GENERIC_WRITE = 0x40000000;
+        private const UInt32 GENERIC_READ = 0x80000000;
+        private const UInt32 FILE_SHARE_READ = 0x00000001;
+        private const UInt32 FILE_SHARE_WRITE = 0x00000002;
+        private const UInt32 OPEN_EXISTING = 0x00000003;
+        private const UInt32 FILE_ATTRIBUTE_NORMAL = 0x80;
+        private const UInt32 ERROR_ACCESS_DENIED = 5;
+
+        private const UInt32 ATTACH_PARRENT = 0xFFFFFFFF;
+
+    #endregion
+    }
+#endif
+
     class Program
     {
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
@@ -17,24 +117,28 @@ namespace FanCtrlTray
         const int CyclesToWaste = (UpdateTime / CycleTime) - 1;
 
         static bool run = true;
+        static bool getFanSpeed = false;
         static IFanCtrlInterface interf = null;
-        static ToolStripMenuItem rpmItem = null;
         static ToolStripMenuItem forceItem = null;
         static ToolStripItem exitItem = null;
 
 
         static void Main(string[] args)
         {
+#if DEBUG
+            WinConsole.Initialize();
+#endif
             ChannelFactory<IFanCtrlInterface> pipeFactory = new ChannelFactory<IFanCtrlInterface>(new NetNamedPipeBinding(NetNamedPipeSecurityMode.None), new EndpointAddress("net.pipe://localhost/FanCtrlInterface"));
 
             ContextMenuStrip strip = new ContextMenuStrip();
 
-            rpmItem = (ToolStripMenuItem)strip.Items.Add("Fan0: RPM");
+            ToolStripMenuItem rpmItem = (ToolStripMenuItem)strip.Items.Add("Fan0: 0 RPM");
             //rpmItem.Enabled = false;
             forceItem = (ToolStripMenuItem)strip.Items.Add("Force full speed");
             strip.Items.Add("-");
             exitItem = strip.Items.Add("Exit");
             strip.Opening += Strip_Opening;
+            strip.Closing += Strip_Closing;
             strip.ItemClicked += Strip_ItemClicked;
 
             NotifyIcon icon = new NotifyIcon();
@@ -51,6 +155,7 @@ namespace FanCtrlTray
             string txt;
             uint fanlvl;
             FanCtrlData d;
+            uint lastFanSpeed = 0;
 
             uint counter = 0;
 
@@ -64,6 +169,19 @@ namespace FanCtrlTray
                             interf = pipeFactory.CreateChannel();
 
                         d = interf.GetData();
+
+                        if (getFanSpeed)
+                        {
+                            uint fanSpeed = interf.GetFan1Rpm();
+#if DEBUG
+                            Console.WriteLine(fanSpeed);
+#endif
+                            if (lastFanSpeed != fanSpeed)
+                            {
+                                lastFanSpeed = fanSpeed;
+                                rpmItem.Text = $"Fan0: {fanSpeed} RPM";
+                            }
+                        }
 
                         if (d.SystemTemperature < 100)
                             txt = d.SystemTemperature.ToString();
@@ -111,7 +229,12 @@ namespace FanCtrlTray
 
         private static void Strip_Opening(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            rpmItem.Text = $"Fan0: {(interf != null ? interf.GetFan1Rpm().ToString() : string.Empty)} RPM";
+            getFanSpeed = true;
+        }
+
+        private static void Strip_Closing(object sender, ToolStripDropDownClosingEventArgs e)
+        {
+            getFanSpeed = false;
         }
 
         private static void Strip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -122,6 +245,7 @@ namespace FanCtrlTray
             }
             else if (e.ClickedItem == forceItem && interf != null)
             {
+                getFanSpeed = false;
                 interf.SetLevel2IsForced(forceItem.Checked = !forceItem.Checked);
             }
         }
